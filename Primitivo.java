ba607e110c323aa2fecbbfe98f1b42d05cf5a677
IsaacAE <alcantara_estrada_kevin@ciencias.unam.mx>
2021-11-09 18:59:09 -0600

    Documentación de Bisiesto.java y Primitivo.java

---
+++ Primitivo.java
@@ -0,0 +1,50 @@
+/**
+ *Clase Primitivo
+ *@author Alcántara Estrada Kevin Isaac
+ * @version 1.0
+ */
+public class Primitivo{
+    /**
+ *@author Alcántara Estrada Kevin Isaac
+ *@method main para imprimir la cantidad de bytes que ocupa cada dato primitivo en java
+ *@param args
+ */
+    public static void main(String[] args){
+	
+	//Imprime en pantalla el título del programa, un previo de lo que se presentará
+	 
+	System.out.println("Datos Primitivos\n" );
+	
+	//Imprime en pantalla la cantidad de bytes que ocupa en la memoria el dato primitivo de tipo byte
+	
+	System.out.println("byte: " + Byte.BYTES + " bytes" );
+	
+	//Imprime en pantalla la cantidad de bytes que ocupa en la memoria el dato primitivo de tipo shor
+	 
+	System.out.println("short: " + Short.BYTES + " bytes");
+
+	//Imprime en pantalla la cantidad de bytes que ocupa en la memoria el dato primitivo de tipo int
+	 
+	System.out.println("int: " + Integer.BYTES + " bytes");
+
+	//Imprime en pantalla la cantidad de bytes que ocupa en la memoria el dato primitivo de tipo long
+	 
+	System.out.println("long: " + Long.BYTES + " bytes");
+
+	//Imprime en pantalla la cantidad de bytes que ocupa en la memoria el dato primitivo de tipo float
+	 
+	System.out.println("float: " + Float.BYTES + " bytes");
+
+	//Imprime en pantalla la cantidad de bytes que ocupa en la memoria el dato primitivo de tipo double
+	 
+	System.out.println("double: " + Double.BYTES + " bytes");
+
+	//Imprime en pantalla la cantidad de bytes que ocupa en la memoria el dato primitivo de tipo boolean
+	 
+	System.out.println("boolean: 1 bytes " );
+
+	//Imprime en pantalla la cantidad de bytes que ocupa en la memoria el dato primitivo de tipo char
+	 
+	System.out.println("char: " + Character.BYTES + " bytes");
+    }
+}
