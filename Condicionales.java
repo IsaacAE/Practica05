5fac45eb48809bc63ee1bcfc7c3833558e69fb06
IsaacAE <alcantara_estrada_kevin@ciencias.unam.mx>
2021-11-09 19:34:18 -0600

    Documentación del archivo Condicionales.java

---
+++ Condicionales.java
@@ -0,0 +1,54 @@
+import java.util.Scanner;
+/**
+ * Clase Condicionales 
+ * @author Alcántara Estrada Kevin Isaac
+ * @version 1.0
+ */
+public class Condicionales{
+    /**
+ * Clase Condicionales 
+ * @author Alcántara Estrada Kevin Isaac
+ * @method Main sirve para decidir cuál de dos números es mayor
+ * @param args
+ */
+    public static void main(String[] args){
+
+	// Se crean dos variables de tipo float, una para cada número a recibir
+ 
+	float num1, num2;
+
+	// Se crea un objeto de la clase Scanner para poder recoger los datos dados por el usuario
+ 
+        Scanner num = new Scanner(System.in);
+
+	// Se solicita en patalla, al usuario, que escriba el valor del primer número
+ 
+	System.out.print("Dame el número 1: ");
+			
+// Se asigna el valor escaneado a la variable num1
+ 
+	num1 = num.nextFloat();
+
+	// Se solicita en patalla, al usuario, que escriba el valor del segundo número
+ 
+	System.out.print("Dame el número 2: ");
+
+	// Se asigna el valor escaneado a la variable num2
+ 
+	num2 = num.nextFloat();
+
+	// Se pregunta si el valor de la variable num1 es mayor al valor de la variable num2
+ 
+	if(num1>num2){
+
+	    // Si se cumple la condición, se imprime en pantalla que el número de mayor valor es el contenido en la variable num1
+ 
+	    System.out.println("El número con mayor valor es " + num1);
+	}else{
+
+	    // Si no  se cumple la condición, se imprime en pantalla que el número de mayor valor es el contenido en la variable num2
+ 
+	    System.out.println("El número con  mayor valor es " + num2);
+	}
+    }
+}
